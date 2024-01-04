97ff571e2767c0ffd18f26535a299c84c5532b33
https://github.com/030439 <praimcahnd763@gmail.com>
2024-01-04 23:30:08 +0500

    head page

---
+++ resources/views/parts/head.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from codeskdhaka.com/html/cashiar-prev/cashiar/biller.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Jan 2024 18:23:28 GMT -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
+  <link rel="stylesheet" href="assets/css/metisMenu.css">
+  <link rel="stylesheet" href="assets/css/swiper-bundle.css">
+  <link rel="stylesheet" href="assets/css/nice-select.css">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <title>Cashiar – Tailwind HTML5 Accounting Dashboard Template</title>
+  <script src="https://cdn.tailwindcss.com/"></script>
+  <script src="assets/js/tailwind-config.js"></script>
+</head>
+
+<body class="bg-bodyBg">

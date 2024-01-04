d8dd434f34d1068b5994092c36b18b1b18ebd160
https://github.com/030439 <praimcahnd763@gmail.com>
2024-01-04 23:35:03 +0500

    file page

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
-  <link rel="stylesheet" href="assets/css/metisMenu.css">
-  <link rel="stylesheet" href="assets/css/swiper-bundle.css">
-  <link rel="stylesheet" href="assets/css/nice-select.css">
-  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="{{ assets('assets/css/font-awesome-pro.css') }}">
+  <link rel="stylesheet" href="{{ assets('assets/css/metisMenu.css') }}">
+  <link rel="stylesheet" href="{{ assets('assets/css/swiper-bundle.css') }}">
+  <link rel="stylesheet" href="{{ assets('assets/css/nice-select.css') }}">
+  <link rel="stylesheet" href="{{ assets('assets/css/main.css') }}">
   <title>Cashiar – Tailwind HTML5 Accounting Dashboard Template</title>
   <script src="https://cdn.tailwindcss.com/"></script>
-  <script src="assets/js/tailwind-config.js"></script>
+  <script src="{{ assets('assets/js/tailwind-config.js') }}"></script>
 </head>
 
 <body class="bg-bodyBg">

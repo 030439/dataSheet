4b5029ca7e3ec84635097126c3b4d6b76919849f
https://github.com/030439 <praimcahnd763@gmail.com>
2024-01-07 05:30:44 +0500

    super admin constrcutr

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function __construct()
+
+    {
+        $this->middleware('auth');
+        $this->middleware('role:ROLE_ADMIN');
+    }
+
     public function index()
     {
         return view('admin.home');

55aa13a9e441431ffb8a61a2af4a3a6e09ceebbc
https://github.com/030439 <praimcahnd763@gmail.com>
2024-01-07 05:25:02 +0500

    checkrole middleware

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -3,18 +3,21 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
 {
-    /**
+   /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next, $role)
     {
+        if (! $request->user()->hasRole($role)) {
+            abort(401, 'This action is unauthorized.');
+        }
         return $next($request);
     }
 }
